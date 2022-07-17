aeb0f5dafc683374c720a3520e6605adb5708d3b
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-07-17 20:21:17 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	   name='quickemcee',
+	   packages=['quickemcee'],
+	   version='0.0.1a0',
+	   description=('quickemcee is a library developed to sum up '
+		        'scripts using the emcee library to quickly set up '
+		        'MCMC analysis, and includes some QOL features.'),
+	   long_description=long_description,
+	   long_description_content_type="text/markdown",
+	   url='https://github.com/sofia-scz/quick-emcee/',
+	   author='Sofia A. Scozziero',
+	   author_email='sgscozziero@gmail.com',
+	   install_requires=['numpy','scipy','matplotlib','emcee','corner'], 
+	   classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Operating System :: POSIX :: Linux",]
+)
