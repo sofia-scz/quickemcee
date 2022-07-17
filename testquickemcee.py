297fa698fbd58ce7935cf4104efb6c0cb5f5d380
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-07-17 19:23:18 -0300

    Update testquickemcee.py

--- testquickemcee.py
+++ testquickemcee.py
@@ -1,8 +1,12 @@
 import quickemcee
 import numpy as np
+from time import sleep
+
+ndata = 201
 
 np.random.seed(335)
-x_data = np.linspace(0, 1, 51) + np.random.uniform(-.01, .01, 51)
+x_data = np.linspace(0, 1, ndata) + np.random.uniform(
+                                            -.1/ndata, .1/ndata, ndata)
 
 
 def f(x, m, h):
@@ -10,12 +14,13 @@ def f(x, m, h):
     return m * x + h
 
 
-y_data = np.array([f(x, .5, 1) + np.random.normal(0, .05) for x in x_data])
+y_data = np.array([f(x, .5, 1) + np.random.normal(0, .1) for x in x_data])
 
 
 def predict(coords):
     """Compute model prediction for a vector in params spaces."""
     m, h = coords
+    sleep(0.0005)
     return np.array([f(x, m, h) for x in x_data])
 
 
@@ -25,9 +30,8 @@ priors = [quickemcee.utils.uniform_prior(0, 5),
 
 model = quickemcee.core.qmcModel(2, predict, priors, y_data, .1)
 
-sampler = model.setup_emcee_sampler(nwalkers=50)
-
-quickemcee.core.run_mcmc_chain(sampler, 300, 1000)
+sampler = model.qmc_run_chain(nwalkers=100, burn_iter=200, main_iter=500,
+                              init_vals=[.1, .1], workers=10)
 
 samples, flat_samples = sampler.get_chain(), sampler.get_chain(flat=True)
 
@@ -36,3 +40,7 @@ labels = ['m', 'h']
 quickemcee.utils.traceplots(samples, labels)
 quickemcee.utils.cornerplots(flat_samples, labels)
 quickemcee.utils.autocplots(flat_samples, labels)
+quickemcee.utils.resultplot(flat_samples, y_data, x_data, predict,
+                            plotmeans=False, plotsamples=100,
+                            dotsize=2.0, linewidth=1.7,
+                            figsize=(7,5))
