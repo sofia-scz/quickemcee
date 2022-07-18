62006dcc40a9f215f88387905a32311aa03b6d94
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-07-18 20:28:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
 	   name='quickemcee',
 	   packages=['quickemcee'],
-	   version='0.0.1a1',
-	   description=('quickemcee is a library developed to sum up '
-		        'scripts using the emcee library to quickly set up '
-		        'MCMC analysis. It includes some QOL features.'),
+	   version='0.0.1a2',
+	   description=('quickemcee is a library with built in scripts'
+		        'using the emcee library to quickly set up '
+			'MCMC analysis. It also includes utilities to '
+		        'process the results.'),
 	   long_description=long_description,
 	   long_description_content_type="text/markdown",
 	   url='https://github.com/sofia-scz/quick-emcee/',
